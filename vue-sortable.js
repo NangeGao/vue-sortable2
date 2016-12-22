17cc26d5f4def9a97a1b3a0587fa3f852fae44a3
gaonan-iri <gaonan-iri@360.cn>
2016-12-22 17:12:37 +0800

    update

---
+++ vue-sortable.js
@@ -0,0 +1,67 @@
+;(function () {
+
+  var vSortable = {}
+  var Sortable = typeof require === 'function'
+      ? require('sortablejs')
+      : window.Sortable
+
+  if (!Sortable) {
+    throw new Error('[vue-sortable] cannot locate Sortable.js.')
+  }
+
+  // exposed global options
+  vSortable.config = {}
+
+  vSortable.install = function (Vue) {
+      Vue.directive('sortable', {
+          bind (el, binding, vnode) {
+
+              console.log(el);
+              console.log(binding);
+              console.log(vnode);
+              //return;
+
+              var data = binding.value;
+              var options = {
+                  onUpdate (evt) {
+                      var sort = [];    //每个位置上存放的item的index
+                      data.forEach((item, index) => {
+                          sort[item.select_sort-1] = index;
+                      });
+                      var operateItem = sort.splice(evt.oldIndex, 1);
+                      sort.splice(evt.newIndex, 0, operateItem[0]);
+                      for(var i=0,len=sort.length; i<len; i++) {
+                          data[sort[i]].select_sort = i+1;
+                      }
+                  }
+              };
+
+              var sortable = new Sortable(el, options)
+
+              if (this.arg && !this.vm.sortable) {
+                  this.vm.sortable = {}
+              }
+
+              //  Throw an error if the given ID is not unique
+              if (this.arg && this.vm.sortable[this.arg]) {
+                  console.warn('[vue-sortable] cannot set already defined sortable id: \'' + this.arg + '\'')
+              } else if( this.arg ) {
+                  this.vm.sortable[this.arg] = sortable
+              }
+          }
+      });
+
+  }
+
+  if (typeof exports == "object") {
+    module.exports = vSortable
+  } else if (typeof define == "function" && define.amd) {
+    define([], function () {
+      return vSortable
+    })
+  } else if (window.Vue) {
+    window.vSortable = vSortable
+    Vue.use(vSortable)
+  }
+
+})()
